957bc02addcd225d00443e0a0bb243cc9cba0fc2
OlaRonning <ronning@pm.me>
2020-07-07 13:22:30 +0200

    ADDED: checkpoint to init for callbacks.

--- numpyro/callbacks/__init__.py
+++ numpyro/callbacks/__init__.py
@@ -3,9 +3,11 @@ from numpyro.callbacks.progbar import Progbar
 from numpyro.callbacks.history import History
 from numpyro.callbacks.early_stopping import EarlyStopping
 from numpyro.callbacks.terminate_on_nan import TerminateOnNaN
+from numpyro.callbacks.checkpoint import Checkpoint
 
 __all__ = [
     'Callback',
+    'Checkpoint',
     'Progbar',
     'History',
     'EarlyStopping',
