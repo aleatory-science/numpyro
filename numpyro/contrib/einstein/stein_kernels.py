fixed the rbf kernel to mimic SVGD implementation from Liu https://github.com/DartML/Stein-Variational-Gradient-Descent/blob/master/python/svgd.py#L10-L14

numpyro/contrib/einstein/stein_kernels.py
@@ -79,7 +79,7 @@ class RBFKernel(SteinKernel):
         self,
         mode="norm",
         matrix_mode="norm_diag",
-        bandwidth_factor: Callable[[float], float] = lambda n: 1 / jnp.log(n),
+        bandwidth_factor: Callable[[float], float] = lambda n: 1 / jnp.log1p(n),
     ):
         assert mode == "norm" or mode == "vector" or mode == "matrix"
         assert matrix_mode == "norm_diag" or matrix_mode == "vector_diag"
@@ -94,10 +94,11 @@ class RBFKernel(SteinKernel):
 
     def compute(self, particles, particle_info, loss_fn):
         bandwidth = median_bandwidth(particles, self.bandwidth_factor)
+        print(bandwidth)
 
         def kernel(x, y):
             reduce = jnp.sum if self._normed() else lambda x: x
-            kernel_res = jnp.exp(-reduce((x - y) ** 2) / stop_gradient(bandwidth))
+            kernel_res = jnp.exp(-0.5 * reduce((x - y) ** 2) / stop_gradient(bandwidth))
             if self._mode == "matrix":
                 if self.matrix_mode == "norm_diag":
                     return kernel_res * jnp.identity(x.shape[0])

numpyro/contrib/einstein/stein_util.py
@@ -29,17 +29,14 @@ def all_pairs_eucl_dist(a, b):
     a_sqr = jnp.sum(a**2, 1)[None, :]
     b_sqr = jnp.sum(b**2, 1)[:, None]
     diff = jnp.matmul(b, a.T)
-    return jnp.sqrt(jnp.maximum(a_sqr + b_sqr - 2 * diff, 0.0))
+    return jnp.maximum(a_sqr + b_sqr - 2 * diff, 0.0)
 
 
 def median_bandwidth(particles, factor_fn):
     if particles.shape[0] == 1:
         return 1.0  # Median produces NaN for single particle
     dists = all_pairs_eucl_dist(particles, particles)
-    bandwidth = (
-        jnp.median(dists) ** 2 * factor_fn(particles.shape[0])
-        + jnp.finfo(dists.dtype).eps
-    )
+    bandwidth = jnp.median(dists) * factor_fn(particles.shape[0]) + jnp.finfo(dists.dtype).eps
     return bandwidth
 
 

numpyro/contrib/einstein/steinvi.py
@@ -288,7 +288,7 @@ class SteinVI:
         )
 
         force = vmap(
-            lambda y: jnp.sum(
+            lambda y: jnp.mean(
                 vmap(
                     lambda x: self.repulsion_temperature
                     * self._kernel_grad(kernel, x, y)
@@ -404,15 +404,15 @@ class SteinVI:
             pinfos=pinfos,
             unravel_fn=unravel_fn,
             uparams=uparams,
-            *args,
-            **kwargs,
+            model_args=args,
+            model_kwargs=kwargs,
         )
         repr_force = self._compute_repulsive(ps=ps, 
                                              pinfos=pinfos, 
                                              unravel_fn=unravel_fn)
 
         particle_grads = (
-            vmap(self._compute_stein_force)(ps, attr_force, repr_force)
+            vmap(partial(self._compute_stein_force, unravel_fn=unravel_fn))(ps, attr_force, repr_force)
             / self.num_stein_particles
         )  # TODO: why divid here again? just scales the gradient
 