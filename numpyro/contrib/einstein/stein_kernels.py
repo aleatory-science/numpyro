46977c4aa0a6ed665d4fd4743ca64b9bffd8ee10
OlaRonning <ronning@pm.me>
2024-06-20 11:46:06 +0200

    updated kernels to be consistent with ADVI -- particles operate in unconstrained space. fixed bug where kernel.compute only used the first particle in particle test.

--- numpyro/contrib/einstein/stein_kernels.py
+++ numpyro/contrib/einstein/stein_kernels.py
@@ -99,9 +99,9 @@ class RBFKernel(SteinKernel):
 
         def kernel(x, y):
             reduce = jnp.sum if self._normed() else lambda x: x
-            kernel_res = jnp.exp(-reduce((x - y) ** 2) / stop_gradient(bandwidth))
+            kernel_res = jnp.exp(-reduce((x - y) ** 2) / (stop_gradient(bandwidth)))
             if self._mode == "matrix":
-                if self.matrix_mode == "norm_diag":
+                if self.matrix_mode == "norm_diag":  # TODO: this case corresponds to SVGD (remove!)
                     return kernel_res * jnp.identity(x.shape[0])
                 else:
                     return jnp.diag(kernel_res)
